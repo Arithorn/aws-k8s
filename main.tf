f0f56bddd9aa9098e706c2e7955b2386e8d68622
Peet Gouws <peetg@discovery.co.za>
2019-02-28 13:25:30 +0200

    changed master to spot instance

--- main.tf
+++ main.tf
@@ -1,19 +1,32 @@
 provider "aws" {
   region = "eu-west-1"
 }
-resource "aws_instance" "k8s-master" {
-  ami = "ami-00035f41c82244dab"
-  instance_type = "t2.micro"
-  key_name = "eu-w-2-terraform"
-  count = 1
-  user_data = "${file("master.conf")}"
+resource "aws_spot_instance_request" "k8s-master" {
+  ami           = "ami-00035f41c82244dab"
+  instance_type = "t3.small"
+  wait_for_fulfillment = true
+  tags = {
+    Name = "master"
+  }
 }
+
+
+# resource "aws_instance" "k8s-master" {
+#   ami = "ami-00035f41c82244dab"
+#   instance_type = "t2.micro"
+#   key_name = "eu-w-2-terraform"
+#   count = 1
+#   user_data = "${file("master.conf")}"
+# }
 resource "aws_instance" "k8s-worker" {
   ami = "ami-00035f41c82244dab"
   instance_type = "t2.micro"
   key_name = "eu-w-2-terraform"
   count = 3
   user_data = "${file("worker.conf")}"
+  tags = {
+      Name = "worker"
+  }
 }
 output "master_ips" {
   value = ["${aws_instance.k8s-master.*.public_ip}"]
