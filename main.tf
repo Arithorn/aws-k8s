15c07d9be436aef7670df9e0a64a99a76219e91a
Peet Gouws <peet@gouws.com>
2019-02-28 11:41:02 +0200

    added clout init

--- main.tf
+++ main.tf
@@ -6,12 +6,14 @@ resource "aws_instance" "k8s-master" {
   instance_type = "t2.micro"
   key_name = "eu-w-2-terraform"
   count = 1
+  user_data = "${file("master.conf")}"
 }
 resource "aws_instance" "k8s-worker" {
   ami = "ami-00035f41c82244dab"
   instance_type = "t2.micro"
   key_name = "eu-w-2-terraform"
   count = 3
+  user_data = "${file("worker.conf")}"
 }
 output "master_ips" {
   value = ["${aws_instance.k8s-master.*.public_ip}"]
