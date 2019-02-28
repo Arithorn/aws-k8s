a507cabfb5d1b80af01c638956f5cbd84b665c83
Peet Gouws <peetg@discovery.co.za>
2019-02-28 11:11:47 +0200

    master and worker nodes

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 provider "aws" {
   region = "eu-west-1"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "k8s-master" {
+  ami = "ami-00035f41c82244dab"
+  instance_type = "t2.micro"
+  key_name = "eu-w-2-terraform"
+  count = 1
+}
+resource "aws_instance" "k8s-worker" {
   ami = "ami-00035f41c82244dab"
   instance_type = "t2.micro"
   key_name = "eu-w-2-terraform"
   count = 3
 }
-output "instance_ips" {
-  value = ["${aws_instance.web.*.public_ip}"]
+output "master_ips" {
+  value = ["${aws_instance.k8s-master.*.public_ip}"]
+}
+output "worker_ips" {
+  value = ["${aws_instance.k8s-worker_ips.*.public_ip}"]
 }
